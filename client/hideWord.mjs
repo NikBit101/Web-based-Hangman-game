904366999edbd1921418429e1002097c7f20e095
NikBit101 <n.cern.03@gmail.com>
2022-03-05 16:59:22 +0000

    added eslint

--- client/hideWord.mjs
+++ client/hideWord.mjs
@@ -1,7 +1,7 @@
 export function hideWord(who) {
-    let hiddenWord = [];
-    for(let i = 0; i < who.length; i+=1) {
-        hiddenWord[i] = who[i].replace(who[i], "_");
-    };
-    return hiddenWord;
+  const hiddenWord = [];
+  for (let i = 0; i < who.length; i += 1) {
+    hiddenWord[i] = who[i].replace(who[i], '_');
+  }
+  return hiddenWord;
 }
