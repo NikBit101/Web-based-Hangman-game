ef0eb24ca609c194dabf708e8aa8d97cc870ec1b
NikBit101 <n.cern.03@gmail.com>
2022-04-01 00:11:13 +0100

    reverted to main commit with changes

--- client/hideWord.mjs
+++
@@ -1,7 +0,0 @@
-export function hideWord(who) {
-  const hiddenWord = [];
-  for (let i = 0; i < who.length; i += 1) {
-    hiddenWord[i] = who[i].replace(who[i], '_');
-  }
-  return hiddenWord;
-}
---
+++ client/keyButtons.mjs
@@ -0,0 +1,19 @@
+'use strict';
+
+import { prepareHandles } from './prepareHandles.mjs';
+
+const handles = prepareHandles();
+
+export function enableKeyButtons() {
+  for (let i = 0; i < handles.letterKey.length; i += 1) {
+    const button = handles.letterKey[i];
+    button.disabled = false;
+  }
+}
+
+export function disableKeyButtons() {
+  for (let i = 0; i < handles.letterKey.length; i += 1) {
+    const button = handles.letterKey[i];
+    button.disabled = true;
+  }
+}
